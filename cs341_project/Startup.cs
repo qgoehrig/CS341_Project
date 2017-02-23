2153e7af0f61115354e916e694980130aa0a7a44
qgoehrig <goehrig.quentin@uwlax.edu>
2017-02-23 11:16:00 -0600

    added tyler's lightweight project

---
+++ cs341_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cs341_project.Startup))]
+namespace cs341_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
