ab854bb46a6ab423137eb3e7e7909cd7e9a1d91d
Jeremy Watson <watson.jeremy@uwlax.edu>
2017-02-25 00:34:09 -0600

    ignore me

--- database_design/create_table_statements.sql
+++ database_design/create_table_statements.sql
@@ -1,22 +1,23 @@
 CREATE TABLE [dbo].[aUser] (
-    [uid]             INT         IDENTITY (1, 1) NOT NULL,
-    [utype]           VARCHAR (8) NOT NULL,
-    [email]           TEXT        NOT NULL,
-    [pword]           TEXT        NOT NULL,
-    [dateActivated]   DATE        NOT NULL,
-    [dateDeactivated] DATETIME        NULL,
-    PRIMARY KEY CLUSTERED ([uid] ASC)
+    [uid]             INT           IDENTITY (1, 1) NOT NULL,
+    [utype]           VARCHAR (8)   NOT NULL,
+    [email]           VARCHAR (100) NOT NULL,
+    [pword]           VARCHAR (60)  NOT NULL,
+    [dateActivated]   DATE          NOT NULL,
+    [dateDeactivated] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([uid] ASC),
+    UNIQUE NONCLUSTERED ([email] ASC)
 );
 
 CREATE TABLE [dbo].[Patient] (
-    [pid]       INT          NOT NULL,
-    [first]     VARCHAR (30) NOT NULL,
-    [last]      VARCHAR (40) NOT NULL,
-    [birthdate] DATE         NOT NULL,
-    [address]   TEXT         NULL,
-    [phonenum]	VARCHAR(14)  NULL,
+    [pid]       INT           NOT NULL,
+    [first]     VARCHAR (30)  NOT NULL,
+    [last]      VARCHAR (40)  NOT NULL,
+    [birthdate] DATE          NOT NULL,
+    [address]   VARCHAR (100) NOT NULL,
+    [phonenum]  VARCHAR (14)  NOT NULL,
     PRIMARY KEY CLUSTERED ([pid] ASC),
-	FOREIGN KEY ([pid]) REFERENCES [dbo].[aUser] ([uid])
+    FOREIGN KEY ([pid]) REFERENCES [dbo].[aUser] ([uid])
 );
 
 CREATE TABLE [dbo].[Employee] (
@@ -28,13 +29,13 @@ CREATE TABLE [dbo].[Employee] (
     FOREIGN KEY ([eid]) REFERENCES [dbo].[aUser] ([uid])
 );
 
-CREATE TABLE [dbo].[Appointment] (
-    [pid]       INT          NOT NULL,
-    [adate]     DATE         NOT NULL,
-    [atime]     TIME (7)     NOT NULL,
-    [atype]     VARCHAR (20) NOT NULL,
-    [notes]     TEXT         NULL,
-    [cancelled] BIT          NULL,
+CREATE TABLE [dbo].[Appointments] (
+    [pid]       INT           NOT NULL,
+    [adate]     DATE          NOT NULL,
+    [atime]     TIME (7)      NOT NULL,
+    [atype]     VARCHAR (20)  NOT NULL,
+    [notes]     VARCHAR (MAX) NULL,
+    [cancelled] BIT           DEFAULT ((0)) NOT NULL,
     PRIMARY KEY CLUSTERED ([pid] ASC, [adate] ASC, [atime] ASC),
     FOREIGN KEY ([pid]) REFERENCES [dbo].[Patient] ([pid])
 );
