96a83ffe19c4e1027d1138610daa3a896d75b0a6
Quentin Goehrig <goehrig.quentin@uwlax.edu>
2017-02-26 11:10:36 -0600

    added updated spa styled project

---
+++ cs341_project/cs341_project/Scripts/Main.js
@@ -0,0 +1,26 @@
+﻿
+var cs341 = angular.module("cs341", ["ngRoute"]);
+
+// configure our routes
+cs341.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+         .when("/", {
+             templateUrl: "Pages/home.htm",
+             controller: "loginController"
+         })
+        // route for the home page
+        .when("/home", {
+            templateUrl: "Pages/home.htm",
+            controller: "loginController"
+        })
+        // route for the about page
+        .when("/login", {
+            templateUrl: "Pages/login.htm",
+            controller: "loginController"
+        })
+     .when("/calendar", {
+         templateUrl: "Pages/calendar.html",
+         controller: "calendarController"
+     })
+});
